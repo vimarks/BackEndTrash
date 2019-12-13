7c312f5f0d797c3ba214c85999c76f78f6c05050
Vincent Marks <violateacher123@gmail.com>
2019-12-13 15:36:04 -0600

    can create trash

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -6,10 +6,9 @@ class AuthController < ApplicationController
     #User#authenticate comes from BCrypt
     if @user && @user.authenticate(user_login_params[:password])
       # encode token comes from ApplicationController
-      puts "hello"
+      puts "Verified User!"
       token = encode_token({ user_id: @user.id })
-      puts token
-      render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+      render json: { user: UserSerializer.new(@user), jwt: token, id: @user.id  }, status: :accepted
     else
       render json: { message: 'Invalid username or password' }, status: :unauthorized
     end
@@ -18,7 +17,6 @@ class AuthController < ApplicationController
   private
 
   def user_login_params
-    # params { user: {username: 'Chandler Bing', password: 'hi' } }
     params.require(:user).permit(:username, :password)
   end
 end
--- app/controllers/trashes_controller.rb
+++ app/controllers/trashes_controller.rb
@@ -1,2 +1,27 @@
 class TrashesController < ApplicationController
+  def create
+
+    bounty = params["bounty"]
+    location_id = params["location_id"]
+    cleaner_id = params["cleaner_id"]
+    cleaned = params["cleaned"]
+    description = params["description"]
+    reporter_id = params["reporter_id"]
+    trash = Trash.new(
+      bounty: bounty,
+      location_id: location_id,
+      cleaner_id: cleaner_id,
+      cleaned: cleaned,
+      description: description,
+      reporter_id: reporter_id
+      )
+    if trash.save
+      render json: {
+      allTrash: Trash.all
+      }
+    end
+
+
+  end
+
 end
