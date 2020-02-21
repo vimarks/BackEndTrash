86c0f22f36d9cf2027d526b65ecf9ed0769febc0
Vincent Marks <violateacher123@gmail.com>
2020-02-21 11:45:59 -0600

    reputations added

--- app/controllers/trashes_controller.rb
+++ app/controllers/trashes_controller.rb
@@ -1,7 +1,7 @@
 class TrashesController < ApplicationController
   def index
 
-    trash = Trash
+    trash = Trash.all
     if trash
       render json: {
         trash: trash,
@@ -9,7 +9,9 @@ class TrashesController < ApplicationController
         .map { |trash| trash.location },
         dirtyTrashLocations: trash.select { |trash| trash.cleaned === "dirty"}
         .map { |trash| trash.location },
-        users: User
+        users: User.all,
+        reputations: Reputation.all
+
 
 
       }
