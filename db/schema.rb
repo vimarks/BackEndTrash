2d907c81b1b91d4f91258e382fac91860b66cdf8
Vincent Marks <violateacher123@gmail.com>
2020-02-26 11:01:46 -0600

    only rate trash once

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_215013) do
+ActiveRecord::Schema.define(version: 2020_02_25_175755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2020_02_19_215013) do
     t.float "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "cleaner_id"
+    t.integer "trash_id"
   end
 
   create_table "trashes", force: :cascade do |t|
